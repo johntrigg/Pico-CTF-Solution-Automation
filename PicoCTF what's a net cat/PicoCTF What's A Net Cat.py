c8e411d722449716979b65fc3d093b67c711182d
johntrigg <tampaviolaplayer@gmail.com>
2022-03-30 19:04:09 -0400

    Added PicoCTF What's A Net Cat Solution

---
+++ PicoCTF what's a net cat/PicoCTF What's A Net Cat.py
@@ -0,0 +1,27 @@
+import os
+import re#regular expression to find the flag
+import pwn #from pwn import remote as brandon
+
+def whatsANetCat():
+    pwn.context.log_level = "error"
+
+    x = os.popen("dir").read()
+
+    print(x)
+
+
+    url = "jupiter.challenges.picoctf.org"
+
+    port = 25103
+
+    r = pwn.remote(url, port)
+
+    output = r.recvall().decode()
+
+    print(output)
+
+    flag = re.findall(r"picoCTF{.*?}", output)[0]#take the output, find the first occurance of the regex, and put it into var flag
+
+    print(flag)
+
+whatsANetCat()
